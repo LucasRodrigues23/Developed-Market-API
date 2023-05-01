a8712c5097edffe808c51d63043b86ec7b9d8a90
Antonio <netoifpe@gmail.com>
2023-05-01 15:52:44 -0300

    feat: create model Address

--- addresses/models.py
+++ addresses/models.py
@@ -3,4 +3,13 @@ import uuid
 
 
 class Address(models.Model):
-    ...
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+
+    country = models.CharField(max_length=30)
+    state = models.CharField(max_length=30)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=30)
+    street = models.CharField(max_length=50)
+    number = models.CharField(max_length=10)
+    cep = models.CharField(max_length=9)
+    complement = models.CharField(max_length=100, null=True, blank=True, default=None)
