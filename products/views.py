e24564f19bf809d05f043c8cf0bab6779ef99c26
Paola <pa_rosa@msn.com>
2023-05-03 09:59:02 -0300

    Feat: Product Permissions

--- products/views.py
+++ products/views.py
@@ -6,13 +6,14 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.generics import ListCreateAPIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import ProductSellerPermission
 
 
 class ProductView(ListCreateAPIView, PageNumberPagination):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [ProductSellerPermission]
     serializer_class = ProductSerializer
-    page_size = 2
+    page_size = 10
     
     def get_queryset(self):
         return Product.objects.all()
