5e2fd934744b862bdbcbeb129c44215c7c3d5ebe
Paola <pa_rosa@msn.com>
2023-05-05 11:07:29 -0300

    feat: filtering products by id, name and category

--- products/views.py
+++ products/views.py
@@ -15,11 +15,29 @@ class CustomPaginationProduct(PageNumberPagination):
 class ProductView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [ProductSellerPermission]
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = CustomPaginationProduct
 
     def get_queryset(self):
-        return Product.objects.all()
+        route_parameter_id = self.request.query_params.get("id")
+        route_parameter_name = self.request.query_params.get("name")
+        route_parameter_category = self.request.query_params.get("category")
+
+        if route_parameter_id:
+            queryset = Product.objects.filter(id__iexact=route_parameter_id)
+            return queryset
+        
+        if route_parameter_name:
+            queryset = Product.objects.filter(name__icontains=route_parameter_name)
+            return queryset
+        
+        if route_parameter_category:
+            queryset = Product.objects.filter(category__iexact=route_parameter_category)
+            return queryset
+        
+        return super().get_queryset()
+    
 
     def perform_create(self, serializer):
         return serializer.save(seller=self.request.user)
