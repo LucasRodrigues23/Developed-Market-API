e49c7b1805c4b22863da59f732313d85165c2820
Antonio <netoifpe@gmail.com>
2023-05-08 14:38:58 -0300

    feat: create the route to display the logged in user's profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework_simplejwt import views as jwt_views
 from . import views
 from addresses.views import AddressCreateView
 
@@ -7,6 +6,7 @@ urlpatterns = [
     path("users/", views.UserCreateView.as_view()),
     path("users/<uuid:pk>/", views.UserDetailView.as_view()),
     path("users/<uuid:user_id>/address/", AddressCreateView.as_view()),
+    path("users/profile/", views.UserProfileView.as_view()),
     path("login/", views.LoginJWTView.as_view()),
     path("token/refresh/", views.MyTokenRefreshView.as_view()),
 ]
