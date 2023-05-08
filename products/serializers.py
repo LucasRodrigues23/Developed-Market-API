27479170c6a063bb227ebd96bfc96690ffc92a29
Paola <pa_rosa@msn.com>
2023-05-08 10:42:29 -0300

    feat: creating validate errors to category of products

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, CategoryOptions
+
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.ChoiceField(choices=CategoryOptions.choices)
+
     class Meta:
         model = Product
         fields = [
@@ -23,6 +26,15 @@ class ProductSerializer(serializers.ModelSerializer):
             "seller_id",
             "is_available_for_sale",
         ]
+    
+    def validate_category(self, value):
+        choices = dict(self.CategoryOptions.choices)
+        if value not in choices:
+            raise serializers.ValidationError(
+                f"The available categories are: {', '.join(choices.values())}"
+            )
+        return value
+        
 
     def create(self, validated_data):
         if validated_data["quantity_stock"] > 0:
